01b4aa9c5a91a1128983c94672bb32372d750e70
shordingercat <tangzhige@gmail.com>
2019-05-20 17:39:59 +0800

    add sip3

---
+++ seroparam/param.go
@@ -0,0 +1,10 @@
+package seroparam
+
+const SIP1 = 130000 //for miner rewards
+
+const SIP2 = 606006 //for flight wallet
+//const SIP2 = 0 //for test
+
+const SIP3 = 940410 //for test net
+
+const VP1 = 829000 //for gpu anti
