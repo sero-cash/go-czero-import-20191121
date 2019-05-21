267835d2d0074c5c6f0fb80b955445f56ae72464
shordingercat <tangzhige@gmail.com>
2019-05-21 14:12:16 +0800

    change sip3

--- cpt/constant.go
+++ cpt/constant.go
@@ -30,6 +30,6 @@ const SIP1 = 130000 //for miner rewards
 const SIP2 = 606006 //for flight wallet
 //const SIP2 = 0 //for test
 
-const SIP3 = 0
+const SIP3 = 880286 //for test net
 
 const VP1 = 829000 //for gpu anti
