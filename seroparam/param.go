b2b91392e03c746feca418c5df7677aae716229c
xiangqh <bourneAc>
2019-06-12 22:50:03 +0800

    update

--- seroparam/param.go
+++ seroparam/param.go
@@ -2,8 +2,8 @@ package seroparam
 
 const SIP1 = 130000 //for miner rewards
 
-//const SIP2 = 606006 //for flight wallet
-const SIP2 = 0 //for test
+const SIP2 = 606006 //for flight wallet
+//const SIP2 = 0 //for test
 
 const SIP3 = 940410 //for test net
 
