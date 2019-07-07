522cc130e9cca173c641e07576e4a1b69fbb078a
shordingercat <tangzhige@gmail.com>
2019-07-08 00:47:07 +0800

    add sip4

--- seroparam/param.go
+++ seroparam/param.go
@@ -40,6 +40,14 @@ func VP0() uint64 {
 	}
 }
 
+func SIP4() uint64 {
+	if is_dev {
+		return 0
+	} else {
+		return uint64(1293528)
+	}
+}
+
 const MAX_O_INS_LENGTH = int(2500)
 
 const MAX_TX_OUT_COUNT_LENGTH = int(256)
