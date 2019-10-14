65926692bb95766d5ef407e2c3853b6d1f60bdb5
shordingercat <tangzhige@gmail.com>
2019-10-14 15:27:39 +0800

    update lib

--- czero/szk_include/csuperzk.h
+++ czero/szk_include/csuperzk.h
@@ -21,6 +21,7 @@ extern void superzk_init_params();
 extern void superzk_init_params_no_circuit();
 extern void superzk_random_pt(unsigned char pt[32]);
 extern void superzk_random_fr(unsigned char fr[32]);
+extern void superzk_force_fr(unsigned char data[32], unsigned char fr[32]);
 extern void superzk_merkle_combine(
     const unsigned char l[32],
     const unsigned char r[32],
