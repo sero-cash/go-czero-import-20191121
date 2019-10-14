12ad2b3f035cd0b8ed237236c913c336bfa6eadf
shordingercat <tangzhige@gmail.com>
2019-10-14 15:24:18 +0800

    fix a bug

--- c_superzk/superzk.go
+++ c_superzk/superzk.go
@@ -30,6 +30,14 @@ func RandomFr() (ret c_type.Uint256) {
 	return
 }
 
+func ForceFr(data *c_type.Uint256) (ret c_type.Uint256) {
+	C.superzk_force_fr(
+		(*C.uchar)(unsafe.Pointer(&data[0])),
+		(*C.uchar)(unsafe.Pointer(&ret[0])),
+	)
+	return
+}
+
 func Combine(l *c_type.Uint256, r *c_type.Uint256) (out c_type.Uint256) {
 	C.superzk_merkle_combine(
 		(*C.uchar)(unsafe.Pointer(&l[0])),
